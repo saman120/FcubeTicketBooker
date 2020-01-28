149a5b85fb4a5c0dcfed909820dec96e1cffdc2a
Saman <samanacharya@gmail.com>
2020-01-27 17:58:07 -0800

    changes

--- src/main/java/com/pcs/show/checker/Launcher.java
+++ src/main/java/com/pcs/show/checker/Launcher.java
@@ -5,7 +5,7 @@ import com.pcs.crawler.SiteCrawler;
 public class Launcher {
 
     public static void main(String[] args) {
-        SiteCrawler siteCrawler = new FcubeCrawler("2018", "05", "01", "752", "11:30 AM");
+        SiteCrawler siteCrawler = new FcubeCrawler("2020", "01", "30", "1039", "12:30 PM");
         siteCrawler.login();
         siteCrawler.crawl();
     }
